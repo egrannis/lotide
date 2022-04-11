72f2bd960debc8b71e2aa027665540b0ce334764
Emma Grannis <grannis.emma@gmail.com>
2022-04-11 19:06:02 +0000

    Refactored file to only include and only export the function, middle

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-// pasting in eqArrays function from previous exercise
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// new function utilizing eqArrays function
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1,array2)) {
-    console.log(`The assertion has passed. ${array1} === ${array2}`);
-  } else {
-    console.log(`The assertion has failed. ${array1} !== ${array2}`);
-  }
-};
-
-
 const middle = function(array) {
   let output = [];
   if (array.length === 1 || array.length === 2) {
@@ -34,14 +11,10 @@ const middle = function(array) {
   return output;
 };
 
-/// TESTS BELOW USING assertArraysEqual function
 
-assertArraysEqual(middle([1,2]), []); // length is 2, first condition
-assertArraysEqual(middle([1]), []); // length is 1, first condition
-assertArraysEqual(middle([1,2,3]), [2]); // odd
-assertArraysEqual(middle([1,2,3,4,5,6,7]), [4]); // odd
-assertArraysEqual(middle([1,2,3,4]), [2,3]);// even
-assertArraysEqual(middle([1,2,3,4,5,6,7,8]), [4, 5]);// even
+module.exports = middle;
+
+
 
 
 

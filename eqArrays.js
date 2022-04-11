c6443c6e8246dddc54ceeba0613b4b7abe3fb852
Emma Grannis <grannis.emma@gmail.com>
2022-04-11 18:35:45 +0000

    Refactored eqArrays file to import assertEqual and export eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
@@ -18,13 +12,10 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-// TESTS BELOW
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
 
-// Rationale I worked through problem with
+//// Rationale I worked through problem with below ////
+
 // take in 2 arrays
 //compare element 1 of first array to element 1 of second array etc.
 // if the arrays aren't the same length, return false

52f230bd4c2d2669b233d453c5134f8305dc094c
Emma Grannis <grannis.emma@gmail.com>
2022-04-11 20:50:40 +0000

    Removed extraneous comment lines from file

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,10 +1,6 @@
 const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
 
-// only if file contains a large # of functions
-// const myFunction = require('../eqArrays'); // importing the whole object as a variable
-// // myFunction.assertEqual(myfunction.eqArrays)... etc.
-
 // TESTS BELOW
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // pass
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // pass
